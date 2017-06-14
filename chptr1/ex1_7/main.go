c6538f62d778c81ec23c982d09535d7b563dbed7
Ian Tracy <ianmtracy@gmail.com>
2017-06-14 13:36:44 -0400

    ex1_7 complete

--- chptr1/ex1_7/main.go
+++ chptr1/ex1_7/main.go
@@ -2,24 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 )
 
 func main() {
-	fmt.Println("test")
 	for _, url := range os.Args[1:] {
 		res, err := http.Get(url)
 		if err != nil {
 			fmt.Fprint(os.Stderr, err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(res.Body)
-		if err != nil {
+		_, err1 := io.Copy(os.Stdout, res.Body)
+		if err1 != nil {
 			fmt.Fprint(os.Stderr, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
 	}
 }
