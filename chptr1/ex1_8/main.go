94e3416dbee9992dde993d79db641066a252e9de
Ian Tracy <ianmtracy@gmail.com>
2017-06-14 13:42:49 -0400

    ex1_8 complete

---
+++ chptr1/ex1_8/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println("test")
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		res, err := http.Get(url)
+		if err != nil {
+			fmt.Fprint(os.Stderr, err)
+			os.Exit(1)
+		}
+		_, err1 := io.Copy(os.Stdout, res.Body)
+		res.Body.Close()
+		if err1 != nil {
+			fmt.Fprint(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+}
