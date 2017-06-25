8af972444b5959be0ca8726e6f4a196bd9214f8c
kid-it <ianmtracy@gmail.com>
2017-06-25 09:27:44 -0400

    start of ex5_2

---
+++ chptr5/ex5_2/main.go
@@ -0,0 +1,35 @@
+// Findlinks1 prints the links in an HTML document read from standard input.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	visit(doc)
+}
+
+func visit(n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				fmt.Println(a.Val)
+			}
+		}
+	}
+	if n.FirstChild != nil {
+		visit(n.FirstChild)
+	}
+	if n.NextSibling != nil {
+		visit(n.NextSibling)
+	}
+	return
+}
