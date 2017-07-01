e4e8bd6b03c04f569d9076b25262b88b996e8d55
Ian Tracy <ianmtracy@gmail.com>
2017-07-01 16:08:15 -0400

    ex5_3 working how I want it to

---
+++ chptr5/ex5_3/main.go
@@ -0,0 +1,46 @@
+// Findlinks1 prints the links in an HTML document read from standard input.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	visit(doc)
+}
+
+func visit(n *html.Node) {
+	// if n.Type == html.ElementNode && n.Data == "a" {
+	// 	for _, a := range n.Attr {
+	// 		if a.Key == "href" {
+	// 			fmt.Println(a.Val)
+	// 		}
+	// 	}
+	// }
+
+	if n.Type == html.ElementNode && (n.Data == "script" || n.Data == "style") {
+		return
+	}
+	if n.Type == html.TextNode {
+		if len(n.Data) > 0 && !strings.HasPrefix(n.Data, "\n  ") {
+			fmt.Printf("%s", n.Data)
+		}
+	}
+
+	if n.FirstChild != nil {
+		visit(n.FirstChild)
+	}
+	if n.NextSibling != nil {
+		visit(n.NextSibling)
+	}
+	return
+}
