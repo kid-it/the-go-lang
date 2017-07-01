d57018d94113c89f0b65ac41e4b1f7447e32eb0c
Ian Tracy <ianmtracy@gmail.com>
2017-07-01 16:55:52 -0400

    removed white space from ex5_3 and .gitignore

--- chptr5/ex5_3/main.go
+++ chptr5/ex5_3/main.go
@@ -19,13 +19,6 @@ func main() {
 }
 
 func visit(n *html.Node) {
-	// if n.Type == html.ElementNode && n.Data == "a" {
-	// 	for _, a := range n.Attr {
-	// 		if a.Key == "href" {
-	// 			fmt.Println(a.Val)
-	// 		}
-	// 	}
-	// }
 
 	if n.Type == html.ElementNode && (n.Data == "script" || n.Data == "style") {
 		return
