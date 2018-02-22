0b65f0145828b9861ba38993487668323bb5072b
Ian Tracy <iatr@hms.se>
2018-02-22 11:43:52 -0500

    unionWith, String, Remove

--- chptr6/6_1/main.go
+++ chptr6/6_1/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 )
 
 //Type IntSet represents an in-memory set
@@ -34,6 +36,70 @@ func (s *IntSet) Has(n int) bool {
 	return (s.words[w] & (1 << b)) != 0
 }
 
-func main() {
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, wFromN := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= t.words[i]
+		} else {
+			s.words = append(s.words, wFromN)
+		}
+	}
+}
 
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteString("{")
+	for i, j := range s.words {
+		if j == 0 {
+			continue
+		}
+
+		for k := 0; k < 64; k++ {
+			if j&(1<<uint(k)) != 0 {
+				if buf.Len() > 1 {
+					buf.WriteString(" ")
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+int(k))
+			}
+		}
+	}
+	buf.WriteString("}")
+
+	return buf.String()
+}
+
+//Len returns the number of set elements
+func (s *IntSet) Len() int {
+	ret := 0
+	for _, j := range s.words {
+		if j == 0 {
+			continue
+		}
+
+		for k := 0; k < 64; k++ {
+			if j&(1<<uint(k)) != 0 {
+				ret++
+			}
+		}
+	}
+	return ret
+}
+
+// Remove will clear set bit, this will not "shrink" the size of words even if all elements are cleared
+func (s *IntSet) Remove(x int) error {
+	w, b := x/64, uint64(x%64)
+
+	if w > len(s.words) {
+		return fmt.Errorf("Size of s.words could not contain %d", x)
+	}
+
+	targetBit := uint64(1 << b)
+	if (s.words[w] & targetBit) == 0 {
+		return fmt.Errorf("set did not contain %d", x)
+	}
+	s.words[w] &= ^targetBit
+	return nil
+}
+
+func main() {
 }
--- chptr6/6_1/main_test.go
+++ chptr6/6_1/main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
+	"os"
 	"testing"
 )
 
@@ -37,4 +39,106 @@ func TestLargeadd(t *testing.T) {
 		t.Errorf("incorrect result %d", s.words[16])
 	}
 
+}
+
+func TestHas(t *testing.T) {
+	s := &IntSet{}
+	r1 := rand.Uint64() % 64
+	s.Add(int(r1))
+
+	for i := 0; i < 64; i++ {
+		if s.Has(i) && i != int(r1) {
+			t.Errorf("has check not working %d %d", i, int(r1))
+		}
+	}
+
+}
+
+func TestUnion(t *testing.T) {
+	s1 := &IntSet{}
+	rand.Seed(int64(os.Getpid()))
+
+	r1 := rand.Uint64() % 64
+	s1.Add(int(r1))
+
+	s2 := &IntSet{}
+	s2.Add(165)
+
+	s1.UnionWith(s2)
+
+	if !s1.Has(int(r1)) {
+		t.Errorf("expected value missing")
+	}
+
+	if !s1.Has(165) {
+		t.Errorf("expected value")
+	}
+}
+
+func TestString(t *testing.T) {
+
+	s1 := &IntSet{}
+	s2 := &IntSet{}
+
+	s1.Add(1)
+	s1.Add(2)
+	s2.Add(164)
+	s1.UnionWith(s2)
+	str := s1.String()
+	ss := "{1 2 164}"
+	if str != ss {
+		t.Errorf("unexpected return string: %s  expected %s", str, ss)
+	}
+}
+
+func TestLen(t *testing.T) {
+
+	s1 := &IntSet{}
+	if s1.Len() != 0 {
+		t.Errorf("before any adds, length should be 0")
+	}
+
+	s2 := &IntSet{}
+
+	s1.Add(1)
+	s1.Add(2)
+	s2.Add(164)
+	s1.UnionWith(s2)
+	res := s1.Len()
+	if res != 3 {
+		t.Errorf("at this point Len=3, %d reported\n", res)
+	}
+
+	s1.Add(2)
+	if res != 3 {
+		t.Errorf("at this point Len=3, %d reported\n", res)
+	}
+
+}
+
+func TestClear(t *testing.T) {
+	s1 := &IntSet{}
+	err := s1.Remove(125)
+	if err == nil {
+		t.Errorf("before any adds, should flag error if user tries to clear x > 64")
+	}
+
+	s1.Add(4)
+	s1.Add(56)
+
+	s1.Remove(4)
+	b := s1.Has(4)
+	if b {
+		fmt.Println(s1.words)
+		t.Errorf("value should be cleared")
+	}
+	b = s1.Has(56)
+	if !b {
+		fmt.Println(s1.words)
+		t.Errorf("value should still be in set")
+	}
+
+	if err = s1.Remove(34); err == nil {
+		t.Errorf("Removing a int not in the set should return error")
+	}
 }
